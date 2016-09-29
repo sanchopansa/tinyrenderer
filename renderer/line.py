1385537ea8f42232cbb68b6c1844769c502cd27e
Valentin Ganev <sanchopansa@gmail.com>
2016-09-29 23:29:32 +0200

    fix line coordinate computation

--- renderer/line.py
+++ renderer/line.py
@@ -8,42 +8,45 @@ def get_line_coords(start, end):
         :returns: tuples of (x, y)-coordinates for the pixels representing the
                   line
     """
-    points = []
+    coords = []
 
-    # To ensure the same sequence of points is returned regardless of the order
-    # in which the start and end points are passed, we always have the point
-    # with the smaller x-coordinate as the first argument for the rest of the
-    # algorithm
-    if start > end:
-        (start, end) = (end, start)
-
-    x0, y0 = start
-    x1, y1 = end
+    x1, y1 = start
+    x2, y2 = end
 
     rotated = False
     # If the line is steep (i.e. y grows faster than x), we rotate the line.
     # The goal is to always end up with a line that has a |slope| <= 1, for the
     # rest of the algorithm
-    if abs(y1 - y0) > abs(x1 - x0):
-        x0, y0 = y0, x0
+    if abs(y2 - y1) > abs(x2 - x1):
         x1, y1 = y1, x1
+        x2, y2 = y2, x2
         rotated = True
 
-    dx = x1 - x0
-    dy = y1 - y0
+    # To ensure the same sequence of points is returned regardless of the order
+    # in which the start and end points are passed, we always have the point
+    # with the smaller x-coordinate as the first argument for the rest of the
+    # algorithm
+    if x1 > x2:
+        (x1, x2) = (x2, x1)
+        (y1, y2) = (y2, y1)
 
-    error = 2 * dy - dx
-    y = y0
+    dx = x2 - x1
+    dy = y2 - y1
+
+    error_inc = abs(2 * dy)
+    error_dec = 2 * dx
+    error = error_inc - dx
+    y = y1
     y_step = 1 if dy > 0 else -1
-    for x in range(x0, x1 + 1):
+    for x in range(x1, x2 + 1):
         # For |slopes| > 1, the line is rotated, so the coordinates need to be
         # swapped accordingly
-        points.append((x, y) if not rotated else (y, x))
+        coords.append((x, y) if not rotated else (y, x))
         if error > 0:
             y += y_step
-            error -= 2 * dx
-        error += 2 * dy
-    return points
+            error -= error_dec
+        error += error_inc
+    return coords
 
 
 if __name__ == "__main__":
@@ -58,7 +61,7 @@ if __name__ == "__main__":
         for x, y in coords:
             # The canvas' (0, 0) is in the top left corner. We want it to be in
             # the bottom left
-            canvas.putpixel((x, 100 - y), color)
+            canvas.putpixel((x, 99 - y), color)
         return canvas
 
     draw(canvas, get_line_coords((13, 20), (80, 40)))
